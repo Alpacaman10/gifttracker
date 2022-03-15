fa066f8a6e2e3c0dae6e284be2cfa05ba431d1c7
anonymous anonymous <iambrent90@gmail.com>
2022-03-15 15:02:59 +0000

    Generate graphiti resources for circles

---
+++ app/resources/circle_resource.rb
@@ -0,0 +1,11 @@
+class CircleResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
